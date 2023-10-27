a75f6ca756c7f81c85ca49a1f2c9df37ecd9c91f
stupidplait <rusyaplait@gmail.com>
2023-10-27 09:23:18 +0300

    threshold

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,6 @@ const observer = new IntersectionObserver((entries) => {
     if (entries[0].isIntersecting) {
         github.style.display = 'flex';
     }
-}, { threshold: 0 });
+}, { threshold: 0.5 });
 
 observer.observe(document.querySelector('.block:nth-child(4)'));
