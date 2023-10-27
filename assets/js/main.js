1fd647121162067846d6b70c17a6dd538059dde2
stupidplait <rusyaplait@gmail.com>
2023-10-27 09:21:20 +0300

    observer

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const github = document.querySelector('a.github');
+
+const observer = new IntersectionObserver((entries) => {
+    if (entries[0].isIntersecting) {
+        github.style.display = 'flex';
+    }
+}, { threshold: 0 });
+
+observer.observe(document.querySelector('.block:nth-child(4)'));
